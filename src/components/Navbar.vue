990095b78da33d4c36e5bd4a46cf4f41d8b460db
subin33 <shtnqls33@gmail.com>
2025-03-19 19:01:41 +0900

    add navbar and icons

---
+++ src/components/Navbar.vue
@@ -0,0 +1,38 @@
+<template>
+  <nav>
+    <h1>로그인</h1>
+    <router-link to="/job-list" class="btn-close">
+      <Icon icon="material-symbols:close-rounded" 
+      width="28" 
+      style="color: #1e1e1e;"/>
+    </router-link>
+  </nav>
+</template>
+  
+<script setup>
+  import { Icon } from '@iconify/vue';
+
+</script>
+  
+<style lang="scss" scoped>
+  nav {
+    position: relative;
+    // background: pink;
+    border-bottom: 1px solid #ccc;
+    width: 100%;
+    height: 44px;
+    padding: 10px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    h1 {
+      font-size: 16px;
+      color: var(--text-color-dark);
+    }
+    .btn-close {
+      position: absolute;
+      left: 15px;
+      text-decoration: none;
+    }
+  }  
+</style>
