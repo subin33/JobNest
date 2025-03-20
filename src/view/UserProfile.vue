ed4910ee27be40aaa3ad3e2001423f10543a9783
subin33 <shtnqls33@gmail.com>
2025-03-20 14:32:08 +0900

    display screen after checking login status

--- src/view/UserProfile.vue
+++ src/view/UserProfile.vue
@@ -1,12 +1,15 @@
 <template>
-  <h1>UserProfile</h1>
+  <div class="container" v-if="isLogin">
+    <h1>UserProfile</h1>
+  </div>
 </template>
 <script setup>
 import supabase from '../supabase';
-import { onMounted } from 'vue';
+import { onMounted , ref} from 'vue';
 import { useRouter } from 'vue-router';
 
 const router = useRouter();
+const isLogin = ref(false); // 화면 표시 상태 변수 
 
 //마운트시 로그인 상태 확인하기 
 onMounted(async() => {
@@ -15,9 +18,11 @@ onMounted(async() => {
 
   if(user) {
     console.log('로그인 상태');
+    isLogin.value = true;
     console.log(user.email);
   } else {
     console.log('로그아웃 상태');
+    isLogin.value = false;
     alert('로그인 후 이용해주세요.');
     router.push('/');
   }
