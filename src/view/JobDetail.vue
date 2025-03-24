907eb07f671d5044836848992347c76427883d96
subin33 <shtnqls33@gmail.com>
2025-03-24 15:01:59 +0900

    JobDetail - fetch selected post

--- src/view/JobDetail.vue
+++ src/view/JobDetail.vue
@@ -1,16 +1,121 @@
 <template>
-  <h1>JobDetail</h1>
+  <figure>
+    <img src="https://placehold.co/640x360" alt="head image">
+  </figure>
+
+  <!-- 상세정보 -->
+  <div class="container" v-if="post">
+    <h2>{{ post.title }}</h2>
+    <p class="top_info">
+      {{ post.company_name }}
+      <span>&middot;</span>
+      {{ post.location }}
+    </p>
+    <p class="pay">
+      {{ post.pay_rule }}: <b>{{ post.pay.toLocaleString() }}</b>
+    </p>
+    <textarea class="desc" rows="8" disabled>{{ post.desc }}</textarea>
+  </div>
+  <!-- 하단 고정 버튼 -->
+  <div class="bottom-btn-group">
+    <button class="btn-tel">전화문의</button>   
+    <button class="btn-apply">지원하기</button>   
+  </div>
 </template>
 
 <script setup>
 import { useRoute } from 'vue-router';
+import supabase from '../supabase';
+import { ref, onMounted } from 'vue';
+import { da } from 'date-fns/locale';
 
 const route = useRoute();
+const id = route.params.id;
+const post = ref(null);
 console.log(route.params.id)
+
+//DB에서 글 가져오기
+onMounted(async () => {
+  const {data, error} = await supabase
+  .from('job_post')
+  .select('*')
+  .eq('id',id)
+  .single()
+
+  post.value = data;
+
+
+  if(error){
+    alert('글 가져오기 실패')
+  }
+
+})
+
 
 </script>
 
-<style>
-  
-</style>
+<style scoped lang="scss">
+figure {
+  aspect-ratio: 16 / 9;
 
+  img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+}
+
+h2 {
+  font-size: 16px;
+  margin-bottom: 5px;
+}
+
+.top_info {
+  font-size: 12px;
+  color: #666;
+  margin-bottom: 16px;
+}
+
+.pay {
+  font-size: 14px;
+  font-weight: bold;
+  color: #444;
+  padding: 10px 0;
+  margin-bottom: 16px;
+}
+
+.desc {
+  width: 100%;
+  padding: 0px;
+  border: none;
+  line-height: 22px;
+  margin-bottom: 10px;
+  outline: none;
+  background: #fff;
+}
+
+.bottom-btn-group {
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+  display: flex;
+  
+  button {
+    width: 50%;
+    border-radius: 0;
+    padding-top: 14px;
+    padding-bottom: 14px;
+    margin: 0;
+    cursor: pointer;
+  }
+  
+  .btn-tel {
+    background-color: var(--main-color-dark);
+  }
+  
+  .btn-apply {
+    background-color: var(--main-color-light);
+  }
+}
+</style>
