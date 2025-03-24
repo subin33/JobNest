b42615dd075ab099f9babc5928a1acc02ddf9e15
subin33 <shtnqls33@gmail.com>
2025-03-24 15:13:34 +0900

    JobDetail - grant access permission

--- src/view/JobDetail.vue
+++ src/view/JobDetail.vue
@@ -1,55 +1,62 @@
 <template>
-  <figure>
-    <img src="https://placehold.co/640x360" alt="head image">
-  </figure>
+  <section v-if="isLogin">
+    <figure>
+      <img src="https://placehold.co/640x360" alt="head image">
+    </figure>
 
-  <!-- 상세정보 -->
-  <div class="container" v-if="post">
-    <h2>{{ post.title }}</h2>
-    <p class="top_info">
-      {{ post.company_name }}
-      <span>&middot;</span>
-      {{ post.location }}
-    </p>
-    <p class="pay">
-      {{ post.pay_rule }}: <b>{{ post.pay.toLocaleString() }}</b>
-    </p>
-    <textarea class="desc" rows="8" disabled>{{ post.desc }}</textarea>
-  </div>
-  <!-- 하단 고정 버튼 -->
-  <div class="bottom-btn-group">
-    <button class="btn-tel">전화문의</button>   
-    <button class="btn-apply">지원하기</button>   
-  </div>
+    <!-- 상세정보 -->
+    <div class="container" v-if="post">
+      <h2>{{ post.title }}</h2>
+      <p class="top_info">
+        {{ post.company_name }}
+        <span>&middot;</span>
+        {{ post.location }}
+      </p>
+      <p class="pay">
+        {{ post.pay_rule }}: <b>{{ post.pay.toLocaleString() }}</b>
+      </p>
+      <textarea class="desc" rows="5" disabled>{{ post.desc }}</textarea>
+    </div>
+    <!-- 하단 고정 버튼 -->
+    <div class="bottom-btn-group">
+      <button class="btn-tel">전화문의</button>
+      <button class="btn-apply">지원하기</button>
+    </div>
+  </section>
 </template>
 
 <script setup>
+import { useAuth } from '../auth/auth';
 import { useRoute } from 'vue-router';
 import supabase from '../supabase';
 import { ref, onMounted } from 'vue';
-import { da } from 'date-fns/locale';
 
+const { isLogin, user, checkLoginStatus } = useAuth(); // 로그인 상태 확인 함수 가져오기
 const route = useRoute();
 const id = route.params.id;
 const post = ref(null);
-console.log(route.params.id)
+console.log(route.params.id);
 
-//DB에서 글 가져오기
+// DB에서 글 가져오기
 onMounted(async () => {
-  const {data, error} = await supabase
-  .from('job_post')
-  .select('*')
-  .eq('id',id)
-  .single()
+  await checkLoginStatus();
 
-  post.value = data;
+  // 로그인 상태일 때만 데이터 가져오기
+  if (user.value) {
+    const { data, error } = await supabase
+      .from('job_post')
+      .select()
+      .eq('id', id)
+      .single()
 
+    post.value = data;
+    console.log(post.value);
 
-  if(error){
-    alert('글 가져오기 실패')
+    if (error) {
+      alert('글 가져오기 실패');
+    }
   }
-
-})
+});
 
 
 </script>
@@ -100,7 +107,7 @@ h2 {
   left: 0;
   width: 100%;
   display: flex;
-  
+
   button {
     width: 50%;
     border-radius: 0;
@@ -109,11 +116,11 @@ h2 {
     margin: 0;
     cursor: pointer;
   }
-  
+
   .btn-tel {
     background-color: var(--main-color-dark);
   }
-  
+
   .btn-apply {
     background-color: var(--main-color-light);
   }
