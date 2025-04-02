9349288bb077ce8abdc46282e54e7ead7e65e70b
subin33 <shtnqls33@gmail.com>
2025-04-02 11:43:14 +0900

    250402 1143

--- src/main.js
+++ src/main.js
@@ -1,13 +1,6 @@
-// main.js
-import { createApp } from "vue";
-import App from "./App.vue";
 import router from "./router/router";
-import { createHead } from "@vueuse/head";
+import { createApp } from "vue";
 import "./style.css";
+import App from "./App.vue";
 
-const app = createApp(App);
-const head = createHead();
-
-app.use(router);
-app.use(head);
-app.mount("#app");
+createApp(App).use(router).mount("#app");
--- vite.config.js
+++ vite.config.js
@@ -1,39 +1,39 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import { VitePWA } from 'vite-plugin-pwa'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     VitePWA({
-      includeAssets: ['vite.svg', 'favicon.ico', 'robots.txt'], // 캐싱될 자료들
-      registerType: 'autoUpdate',
+      includeAssets: ["vite.svg", "favicon.ico", "robots.txt"], // 캐싱될 자료들
+      registerType: "autoUpdate",
       manifest: {
-        name: '알바잇',
-        short_name: '알바잇',
-        description: '쉽고 편리한 구인구직 서비스',
-        theme_color: '#ffffff',
+        name: "알바잇",
+        short_name: "알바잇",
+        description: "쉽고 편리한 구인구직 서비스",
+        theme_color: "#ffffff",
         icons: [
           {
-            src: 'icon-192x192.png',
-            sizes: '192x192',
-            type: 'image/png'
+            src: "icon-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
           },
           {
-            src: 'icon-512x512.png',
-            sizes: '512x512',
-            type: 'image/png'
-          }
-        ]
-      }
-    })
+            src: "icon-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
   ],
   css: {
     preprocessorOptions: {
       scss: {
-        api: 'modern' // or "modern-compiler"
-      }
-    }
-  }
-})
+        api: "modern", // or "modern-compiler"
+      },
+    },
+  },
+});
